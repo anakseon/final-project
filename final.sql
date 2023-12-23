2c0cb54e320dafa3e21a15699c92519365de19f5
anakseon <127014911+anakseon@users.noreply.github.com>
2023-12-23 20:23:34 +0300

    Add files via upload

--- final.sql
+++ final.sql
@@ -76,5 +76,66 @@ VALUES (1, 'German Shepherd', 5);
 
 INSERT INTO Cats (cat_id, breed, age)
 VALUES (1, 'Persian', 3);
+INSERT INTO hamsters (Name, Birthday, Commands, Genus_id)
+VALUES ('Малой', '2020-10-12', '', 3),
+('Медведь', '2021-03-12', "атака сверху", 3),  
+('Ниндзя', '2022-07-11', NULL, 3), 
+('Бурый', '2022-05-10', NULL, 3);
 
+INSERT INTO horses (Name, Birthday, Commands, Genus_id)
+VALUES ('Гром', '2020-01-12', 'бегом, шагом', 1),
+('Закат', '2017-03-12', "бегом, шагом, хоп", 1),  
+('Байкал', '2016-07-12', "бегом, шагом, хоп, брр", 1), 
+('Молния', '2020-11-10', "бегом, шагом, хоп", 1);
+
+INSERT INTO donkeys (Name, Birthday, Commands, Genus_id)
+VALUES ('Первый', '2019-04-10', NULL, 2),
+('Второй', '2020-03-12', "", 2),  
+('Третий', '2021-07-12', "", 2), 
+('Четвертый', '2022-12-10', NULL, 2);
+
+INSERT INTO camels (Name, Birthday, Commands, Genus_id)
+VALUES ('Горбатый', '2022-04-10', 'вернись', 3),
+('Самец', '2019-03-12', "остановись", 3),  
+('Сифон', '2015-07-12', "повернись", 3), 
+('Борода', '2022-12-10', "улыбнись", 3);
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM camels;
+
+
+CREATE TABLE новая_таблица AS
+SELECT * FROM horses
+UNION
+SELECT * FROM camels;
+
+CREATE TABLE молодые_животные AS
+SELECT * 
+FROM Domestic_Animals
+WHERE возраст > 1 AND возраст < 3;
+
+SELECT h.Name, h.Birthday, h.Commands, pa.Genus_name, ya.Age_in_month 
+FROM horses h
+LEFT JOIN yang_animal ya ON ya.Name = h.Name
+LEFT JOIN packed_animals pa ON pa.Id = h.Genus_id
+UNION 
+SELECT d.Name, d.Birthday, d.Commands, pa.Genus_name, ya.Age_in_month 
+FROM donkeys d 
+LEFT JOIN yang_animal ya ON ya.Name = d.Name
+LEFT JOIN packed_animals pa ON pa.Id = d.Genus_id
+UNION
+SELECT c.Name, c.Birthday, c.Commands, ha.Genus_name, ya.Age_in_month 
+FROM cats c
+LEFT JOIN yang_animal ya ON ya.Name = c.Name
+LEFT JOIN home_animals ha ON ha.Id = c.Genus_id
+UNION
+SELECT d.Name, d.Birthday, d.Commands, ha.Genus_name, ya.Age_in_month 
+FROM dogs d
+LEFT JOIN yang_animal ya ON ya.Name = d.Name
+LEFT JOIN home_animals ha ON ha.Id = d.Genus_id
+UNION
+SELECT hm.Name, hm.Birthday, hm.Commands, ha.Genus_name, ya.Age_in_month 
+FROM hamsters hm
+LEFT JOIN yang_animal ya ON ya.Name = hm.Name
+LEFT JOIN home_animals ha ON ha.Id = hm.Genus_id;
 
